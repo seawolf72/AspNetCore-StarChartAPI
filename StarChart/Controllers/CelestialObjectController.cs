a8a4c643e62461505ab2313e4b0b768f9e98cac3
Steve Worrel <steve_worrel@carmax.com>
2019-01-30 10:55:02 -0500

    ch final again

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -67,7 +67,7 @@ namespace StarChart.Controllers
             return CreatedAtRoute("GetById", new { id = celestialObject.Id }, celestialObject);
         }
 
-        [HttpGet("{id}")]
+        [HttpPut("{id}")]
         public IActionResult Update(int id, CelestialObject celestialObject)
         {
             var existingObject = _context.CelestialObjects.Find(id);
